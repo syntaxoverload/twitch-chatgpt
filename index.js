32a99f30105502a7c49b5fdbdab659d646a24ef8
Steven Surujballi <stevesurujballi@gmail.com>
2023-05-11 09:51:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -107,8 +107,8 @@ app.get('/gpt/:text', async (req, res) => {
       const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: prompt,
-        temperature: 0.9,
-        max_tokens: 75,
+        temperature: 0.5,
+        max_tokens: 128,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
