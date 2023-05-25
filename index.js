3bc271eeb1c7000d42dff4f38e0276cf08517b75
Steven Surujballi <stevesurujballi@gmail.com>
2023-05-25 12:29:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get('/gpt/:text', async (req, res) => {
         model: "gpt-3.5-turbo",
         messages: messages,
         temperature: 0.5,
-        max_tokens: 60,
+        max_tokens: 80,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
@@ -88,9 +88,9 @@ app.get('/gpt/:text', async (req, res) => {
         messages.push({role: "assistant", content: agent_response})
 
         //Check for Twitch max. chat message length limit and slice if needed
-        if(agent_response.length > 399){
-          console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
-          agent_response = agent_response.substring(0, 399)
+        if(agent_response.length > 250){
+          console.log("Agent answer exceeds twitch chat limit. Slicing to first 250 characters.")
+          agent_response = agent_response.substring(0, 250)
           console.log ("Sliced agent answer: " + agent_response)
         }
 
