086cfbf957405489924243318e45bc833cba4753
Steven Surujballi <stevesurujballi@gmail.com>
2023-06-08 08:25:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get('/gpt/:text', async (req, res) => {
         model: "gpt-3.5-turbo",
         messages: messages,
         temperature: 1.0,
-        max_tokens: 128,
+        max_tokens: 90,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
